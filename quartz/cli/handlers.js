864fab5c95ab33b18d9217753317a0b0eee0d73a
tmcelroy2202 <tommy.a.mcelroy@gmail.com>
2024-05-13 08:36:35 -0400

    Quartz sync: May 13, 2024, 8:36 AM

--- quartz/cli/handlers.js
+++ quartz/cli/handlers.js
@@ -531,7 +531,7 @@ export async function handleSync(argv) {
   await popContentFolder(contentFolder)
   if (argv.push) {
     console.log("Pushing your changes")
-    const res = spawnSync("git", ["push", "-uf", ORIGIN_NAME, QUARTZ_SOURCE_BRANCH], {
+    const res = spawnSync("git", ["push", "origin"], {
       stdio: "inherit",
     })
     if (res.status !== 0) {
